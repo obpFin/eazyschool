5401ea6cf94adf95ddba60384014763dc347d64a
Oskari Peltonen <oskari.peltonen@outlook.com>
2022-07-27 08:46:41 +0300

    Add thymeleaf template for home

--- src/main/java/com/eazyschool/controller/HomeController.java
+++ src/main/java/com/eazyschool/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.eazyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
-    public String displayHomePage() {
+    @RequestMapping(value = {"", "/", "/home"})
+    public String displayHomePage(Model model) {
+    model.addAttribute("username", "John Doe");
         return "home.html";
     }
 }
