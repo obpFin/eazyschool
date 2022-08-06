d795f1888f15c6c22b2013820b9e9b0c60b49164
Oskari Peltonen <oskari.peltonen@outlook.com>
2022-08-06 09:17:40 +0300

    add csrf

--- src/main/java/com/eazyschool/config/SecurityConfiguration.java
+++ src/main/java/com/eazyschool/config/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf().and()
             .authorizeHttpRequests(
                 (authz) -> {
     /*              try {
