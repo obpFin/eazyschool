9de063a92c434a4b8b7742c24e669ef2a10583e7
Oskari Peltonen <oskari.peltonen@outlook.com>
2022-08-06 08:35:00 +0300

    add login page

--- src/main/java/com/eazyschool/config/SecurityConfiguration.java
+++ src/main/java/com/eazyschool/config/SecurityConfiguration.java
@@ -3,7 +3,12 @@ package com.eazyschool.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -23,13 +28,18 @@ public class SecurityConfiguration {
                   }*/
                     try {
                         authz
+                            .mvcMatchers("/dashboard").authenticated()
                             .mvcMatchers("/home").permitAll()
                             .mvcMatchers("/holidays/**").permitAll()
                             .mvcMatchers("/contact").permitAll()
                             .mvcMatchers("/saveMsg").permitAll()
                             .mvcMatchers("/courses").permitAll()
-                            .mvcMatchers("about").permitAll()
-                            .and().formLogin().and().httpBasic();
+                            .mvcMatchers("/about").permitAll()
+                            .mvcMatchers("/login").permitAll()
+                            .and().formLogin().loginPage("/login")
+                            .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
+                            .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true ).permitAll()
+                            .and().httpBasic();
                     } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
@@ -39,5 +49,22 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-}
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        ArrayList<UserDetails> users = new ArrayList<>();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails adminUser = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("USER", "ADMIN")
+                .build();
+        users.add(user);
+        users.add(adminUser);
+        return new InMemoryUserDetailsManager(users);
+    }
 
+}
