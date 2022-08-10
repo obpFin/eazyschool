a65e2174f9e2f7fae133f31a5686230f144bc8ad
Oskari Peltonen <oskari.peltonen@outlook.com>
2022-08-10 16:05:21 +0300

    transform HolidaysRepository to JPA

--- src/main/java/com/eazyschool/controller/HolidaysController.java
+++ src/main/java/com/eazyschool/controller/HolidaysController.java
@@ -7,14 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 public class HolidaysController {
@@ -33,11 +29,14 @@ public class HolidaysController {
             model.addAttribute("festival", true);
         }
 
-        List<Holiday> holidays = holidaysRepository.findAllHolidays();
+        Iterable<Holiday> holidays = holidaysRepository.findAll();
+        List<Holiday> holidayList = StreamSupport.stream(holidays.spliterator(), false)
+                .collect(Collectors.toList());
+
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
             model.addAttribute(type.toString(),
-                    (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
+                    (holidayList.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
         }
         return "holidays.html";
     }
