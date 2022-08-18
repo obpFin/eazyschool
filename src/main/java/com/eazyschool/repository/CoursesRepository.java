e5abb7881c392539e8d082eab0357d8d3faa3edf
Oskari Peltonen <oskari.peltonen@outlook.com>
2022-08-18 09:29:39 +0300

    add courses for persons

---
+++ src/main/java/com/eazyschool/repository/CoursesRepository.java
@@ -0,0 +1,10 @@
+package com.eazyschool.repository;
+
+import com.eazyschool.model.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoursesRepository extends JpaRepository<Courses, Integer> {
+
+}
