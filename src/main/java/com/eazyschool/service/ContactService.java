89f00ec68acffe826105d67d3a5adbcf6a5ebdc9
Oskari Peltonen <oskari.peltonen@outlook.com>
2022-08-20 09:20:45 +0300

    add dynamic sorting and pagination for messages

--- src/main/java/com/eazyschool/service/ContactService.java
+++ src/main/java/com/eazyschool/service/ContactService.java
@@ -5,6 +5,10 @@ import com.eazyschool.model.Contact;
 import com.eazyschool.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -36,9 +40,12 @@ public class ContactService {
     return isSaved;
     }
 
-    public List<Contact> findMsgsWithOpenStatus() {
-      List<Contact> contactMsgs = contactRepository.findByStatus(EazyschoolConstants.OPEN);
-      return contactMsgs;
+    public Page<Contact> findMsgsWithOpenStatus(int pageNum, String sortField, String sortDir) {
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize,
+                sortDir.equals("asc") ? Sort.by(sortField).ascending() : Sort.by(sortField).descending());
+        Page<Contact> msgPage = contactRepository.findByStatus(EazyschoolConstants.OPEN, pageable);
+        return msgPage;
     }
 
     public boolean updateMsgStatus(int contactId) {
