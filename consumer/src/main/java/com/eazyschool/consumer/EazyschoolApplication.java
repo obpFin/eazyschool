6b586c35adbb8be2df50817b2b3894c66aad7a47
Oskari Peltonen <oskari.peltonen@outlook.com>
2022-08-23 13:49:34 +0300

    setup consumer project

---
+++ consumer/src/main/java/com/eazyschool/consumer/EazyschoolApplication.java
@@ -0,0 +1,15 @@
+package com.eazyschool.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.eazyschool.consumer")
+public class EazyschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyschoolApplication.class, args);
+	}
+
+}
